4c8d61252d75d24ff78286d5eeb31b6e73f63bb9
Arnavkesari <arnavkesari13@gmail.com>
2025-08-15 01:04:31 +0530

    updated structure

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,8 +1,8 @@
-import { asyncHandler } from "../../utils/asynchandler.js";
-import { ApiError } from "../../utils/ApiError.js";
+import { asyncHandler } from "../utils/asynchandler.js";
+import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
-import { uploadOnCloudinary } from "../../utils/Cloudinary.js";
-import { ApiResponse } from "../../utils/ApiResponse.js";
+import { uploadOnCloudinary } from "../utils/Cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 
 const generateAccessAndRefreshTokens = async (userId) => {
